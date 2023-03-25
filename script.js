d3b990326f88bc390016a21d0d7ffcac0af072ef
sideeq12 <abdwaheed2018@gmail.com>
2023-03-25 17:40:42 +0100

    toggle functionality

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const display = document.querySelector('#display');
 const buttons = document.querySelectorAll('button');
 
+
+// In this section, i assigned the svg to two different variable which would be used when toggle button is clicked
+let active_svg = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#000" class="bi bi-moon-stars" viewBox="0 0 16 16">
+<path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278zM4.858 1.311A7.269 7.269 0 0 0 1.025 7.71c0 4.02 3.279 7.276 7.319 7.276a7.316 7.316 0 0 0 5.205-2.162c-.337.042-.68.063-1.029.063-4.61 0-8.343-3.714-8.343-8.29 0-1.167.242-2.278.681-3.286z"/>
+<path d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.734 1.734 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.734 1.734 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.734 1.734 0 0 0 1.097-1.097l.387-1.162zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L13.863.1z"/>
+</svg>`
+let low_svg = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#fff" class="bi bi-brightness-low" viewBox="0 0 16 16">
+<path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zm.5-9.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm0 11a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm5-5a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1zm-11 0a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1zm9.743-4.036a.5.5 0 1 1-.707-.707.5.5 0 0 1 .707.707zm-7.779 7.779a.5.5 0 1 1-.707-.707.5.5 0 0 1 .707.707zm7.072 0a.5.5 0 1 1 .707-.707.5.5 0 0 1-.707.707zM3.757 4.464a.5.5 0 1 1 .707-.707.5.5 0 0 1-.707.707z"/>
+</svg>`
+
 buttons.forEach((item) => {
     item.onclick = () => {
         if (item.id == 'clear') {
@@ -21,10 +31,16 @@ buttons.forEach((item) => {
 const toggleThemeBtn = document.querySelector('.toggle-theme');
 const calculator = document.querySelector('.calculator');
 const toggleIcon = document.querySelector('.toggleIcon');
+toggleThemeBtn.innerHTML = active_svg;
 let isDark = true;
 toggleThemeBtn.onclick = () => {
     calculator.classList.toggle('dark');
     toggleThemeBtn.classList.toggle('active');
     isDark = !isDark;
 
+    // tenary operate used to change the svg icon, if isDark is true, active_svg would show otherwise low_svg would be picked
+    toggleThemeBtn.innerHTML = isDark ? active_svg : low_svg;
 }
+
+
+
